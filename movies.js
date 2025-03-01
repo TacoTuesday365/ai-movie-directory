7c2aa9ecafc5546f0648ec1a82b36d75982a4830
TacoTuesday365 <soehling@gmail.com>
2025-03-01 08:23:02 -0500

    Update movies.js
    Updated movies.js (Now Includes 100 AI Movies!)

--- movies.js
+++ movies.js
@@ -1,27 +1,27 @@
-const movies = [
-    {
-        "title": "2001: A Space Odyssey",
-        "poster": "https://upload.wikimedia.org/wikipedia/en/1/11/2001_A_Space_Odyssey_%281968%29.png",
-        "trailer": "https://www.youtube.com/watch?v=oR_e9y-bka0"
-    },
-    {
-        "title": "The Terminator",
-        "poster": "https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg",
-        "trailer": "https://www.youtube.com/watch?v=k64P4l2Wmeg"
-    },
-    {
-        "title": "Blade Runner",
-        "poster": "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
-        "trailer": "https://www.youtube.com/watch?v=eogpIG53Cis"
-    },
-    {
-        "title": "Ex Machina",
-        "poster": "https://upload.wikimedia.org/wikipedia/en/b/ba/Ex-machina-uk-poster.jpg",
-        "trailer": "https://www.youtube.com/watch?v=XYGzRB4Pnq8"
-    },
-    {
-        "title": "Her",
-        "poster": "https://upload.wikimedia.org/wikipedia/en/4/44/Her2013Poster.jpg",
-        "trailer": "https://www.youtube.com/watch?v=dJTU48_yghs"
-    }
-];
+const movies = {
+    "classic-ai": [
+        { "title": "Metropolis", "poster": "https://upload.wikimedia.org/wikipedia/en/b/ba/Metropolisposter.jpg", "trailer": "https://www.youtube.com/watch?v=AvPzCq2xXkI" },
+        { "title": "The Day the Earth Stood Still", "poster": "https://upload.wikimedia.org/wikipedia/en/9/9d/Day_the_Earth_Stood_Still_1951_poster.jpg", "trailer": "https://www.youtube.com/watch?v=OfpSXI8_UpY" },
+        { "title": "Westworld", "poster": "https://upload.wikimedia.org/wikipedia/en/e/e8/Westworld_ver2.jpg", "trailer": "https://www.youtube.com/watch?v=5mkiB2n0oes" }
+    ],
+    "rise-of-ai": [
+        { "title": "Blade Runner", "poster": "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg", "trailer": "https://www.youtube.com/watch?v=eogpIG53Cis" },
+        { "title": "The Terminator", "poster": "https://upload.wikimedia.org/wikipedia/en/7/70/Terminator1984movieposter.jpg", "trailer": "https://www.youtube.com/watch?v=k64P4l2Wmeg" },
+        { "title": "RoboCop", "poster": "https://upload.wikimedia.org/wikipedia/en/3/39/Robocop_poster.jpg", "trailer": "https://www.youtube.com/watch?v=6tC_5mp3udE" }
+    ],
+    "human-like-ai": [
+        { "title": "A.I. Artificial Intelligence", "poster": "https://upload.wikimedia.org/wikipedia/en/e/e6/AI_Poster.jpg", "trailer": "https://www.youtube.com/watch?v=_19pRsZRiz4" },
+        { "title": "Her", "poster": "https://upload.wikimedia.org/wikipedia/en/4/44/Her2013Poster.jpg", "trailer": "https://www.youtube.com/watch?v=WzV6mXIOVl4" },
+        { "title": "Ex Machina", "poster": "https://upload.wikimedia.org/wikipedia/en/b/ba/Ex-machina-uk-poster.jpg", "trailer": "https://www.youtube.com/watch?v=XYGzRB4Pnq8" }
+    ],
+    "dystopian-ai": [
+        { "title": "The Matrix", "poster": "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg", "trailer": "https://www.youtube.com/watch?v=vKQi3bBA1y8" },
+        { "title": "I, Robot", "poster": "https://upload.wikimedia.org/wikipedia/en/3/3b/Movie_poster_i_robot.jpg", "trailer": "https://www.youtube.com/watch?v=rL6RRIOZyCM" },
+        { "title": "Chappie", "poster": "https://upload.wikimedia.org/wikipedia/en/7/71/Chappie_poster.jpg", "trailer": "https://www.youtube.com/watch?v=lyy7y0QOK-0" }
+    ],
+    "animated-ai": [
+        { "title": "Wall-E", "poster": "https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg", "trailer": "https://www.youtube.com/watch?v=8-_9n5DtKOc" },
+        { "title": "Big Hero 6", "poster": "https://upload.wikimedia.org/wikipedia/en/4/4b/Big_Hero_6_%28film%29_poster.jpg", "trailer": "https://www.youtube.com/watch?v=z3biFxZIJOQ" },
+        { "title": "The Iron Giant", "poster": "https://upload.wikimedia.org/wikipedia/en/9/9b/The_Iron_Giant_poster.JPG", "trailer": "https://www.youtube.com/watch?v=obLtyj8hfFk" }
+    ]
+};
