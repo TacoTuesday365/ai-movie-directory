ae049d4bb46e3e98c8945a8e57eda8f187267722
TacoTuesday365 <soehling@gmail.com>
2025-03-04 13:20:48 -0500

    Create netlify.js

---
+++ netlify.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const apiKey = process.env.OMDB_API_KEY;
+
+    if (!apiKey) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Missing API key' }),
+        };
+    }
+
+    try {
+        const title = event.queryStringParameters.title; // get the title
+        const year = event.queryStringParameters.year; // get the year
+        const apiUrl = `https://www.omdbapi.com/?t=${encodeURIComponent(title)}&y=${year}&apikey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        const data = await response.json();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
+    }
+};
